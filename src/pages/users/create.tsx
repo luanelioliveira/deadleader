d7a4b69ccf70a046965424c6f89300c9f5aa28de
Luan Oliveira <9741553+luanelioliveira@users.noreply.github.com>
2022-08-05 10:57:58 -0300

    configure reponsive page

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -15,22 +15,23 @@ export default function CreateUser() {
       <Flex w="100%" my={6} maxWidth={1480} mx="auto" px={6}>
         <Sidebar/>
 
-        <Box flex={1} borderRadius={8} bg="gray.800" p={8}>
+        <Box flex={1} borderRadius={8} bg="gray.800" p={[6, 8]}>
           <Heading size="lg" fontWeight="normal">Create User</Heading>
+          
           <Divider my={6} borderColor="gray.700" />
           
           <VStack spacing={8}>
-            <SimpleGrid minChildWidth="240px" spacing={8} w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={[6, 8]} w="100%">
               <Input name="name" label="First Name" />
               <Input name="name" label="Last Name" />
               
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing={8} w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={[6, 8]} w="100%">
               <Input name="email" type="email" label="Email"  />
             </SimpleGrid>
             
-            <SimpleGrid minChildWidth="240px" spacing={8} w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={[6, 8]} w="100%">
               <Input name="password" type="password" label="Password" />
               <Input name="passwordConfirmation" type="password" label="Password Confirmation" />
             </SimpleGrid>
