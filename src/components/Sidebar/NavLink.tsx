b012dddd9f929f7c9057415156a32d47e21bdd98
Luan Oliveira <9741553+luanelioliveira@users.noreply.github.com>
2022-08-05 14:34:15 -0300

    fix typescript version

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,17 +1,21 @@
 import { ElementType } from "react";
 
-import { Icon, Link, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react"
+import { Icon, Link as ChakraLink, Text, LinkProps as ChakraLinkProps } from "@chakra-ui/react"
+import Link from "next/link";
 
 interface NavLinkProps extends ChakraLinkProps {
   label: string
   icon: ElementType
+  href: string
 }
 
-export function NavLink({ label, icon, ...rest }: NavLinkProps) {
+export function NavLink({ label, icon, href, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" alignItems="center"{...rest}>
-      <Icon as={icon} />
-      <Text ml={4} fontWeight="medium">{label}</Text>
+    <Link href={href}>
+      <ChakraLink display="flex" alignItems="center"{...rest}>
+        <Icon as={icon} />
+        <Text ml={4} fontWeight="medium">{label}</Text>
+      </ChakraLink>
     </Link>
   )
 }
--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -8,12 +8,12 @@ export function SidebarNav() {
   return (
     <Stack spacing={12} align="flex-start">
       <NavSection title="MAIN">
-        <NavLink label="Dashboard" icon={RiDashboardLine} />    
-        <NavLink label="Users" icon={RiContactsLine} />
+        <NavLink label="Dashboard" icon={RiDashboardLine} href="/dashboard" />    
+        <NavLink label="Users" icon={RiContactsLine} href="/users" />
       </NavSection>
       <NavSection title="AUTOMATION">     
-        <NavLink label="Forms" icon={RiInputMethodLine} />    
-        <NavLink label="Automation" icon={RiGitMergeLine} />
+        <NavLink label="Forms" icon={RiInputMethodLine} href="/" />    
+        <NavLink label="Automation" icon={RiGitMergeLine} href="/" />
       </NavSection>
     </Stack>
   );
