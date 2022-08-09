684a178161c204f5a1cc7077ec216be8372662a1
Luan Oliveira <9741553+luanelioliveira@users.noreply.github.com>
2022-08-09 09:31:12 -0300

    update leaderboard ranking

--- src/components/Leaderboard/index.tsx
+++ src/components/Leaderboard/index.tsx
@@ -19,39 +19,7 @@ import {
 
 import team12Logo from "../../assets/team12.jpeg";
 
-const players = [
-  { alias: "Racco", name: "Jonatas", points: 140, categorie: "rx" },
-  { alias: "Fano", name: "Estafano", points: 135, categorie: "rx" },
-  { alias: "Anão", name: "Diego", points: 130, categorie: "rx" },
-  { alias: "AeroXimiti", name: "Guilherme", points: 125, categorie: "rx" },
-  { alias: "Steffens", name: "Rafael", points: 120, categorie: "rx" },
-  { alias: "Moreli", name: "Bruno", points: 115, categorie: "rx" },
-  { alias: "Frank", name: "Rodrigo", points: 110, categorie: "rx" },
-  { alias: "Pepe", name: "Felipe", points: 105, categorie: "rx" },
-  { alias: "Show", name: "Bruno", points: 100, categorie: "rx" },
-  { alias: "Sagin", name: "Paula", points: 95, categorie: "rx" },
-  { alias: "Lari", name: "Larissa", points: 90, categorie: "rx" },
-  { alias: "Coach", name: "Luiz", points: 100, categorie: "rx" },
-  { alias: "Metal", name: "Camila", points: 90, categorie: "rx" },
-  { alias: "Kapoti", name: "Bruno", points: 85, categorie: "intermediate" },
-  { alias: "Delai", name: "Fran", points: 80, categorie: "intermediate" },
-  { alias: "Martineli", name: "Diego", points: 75, categorie: "intermediate" },
-  { alias: "Coach", name: "Michel", points: 70, categorie: "intermediate" },
-  { alias: "Desafio", name: "Rodrigo", points: 70, categorie: "intermediate" },
-  { alias: "Guima", name: "Guilherme", points: 70, categorie: "intermediate" },
-  { alias: "Delai", name: "Diogo", points: 70, categorie: "intermediate" },
-  { alias: "Silva", name: "Sabrina", points: 70, categorie: "intermediate" },
-  { alias: "Bandeira", name: "Aline", points: 70, categorie: "intermediate" },
-  {
-    alias: "CapotaAberta",
-    name: "João",
-    points: 70,
-    categorie: "intermediate",
-  },
-  { alias: "Bebidas", name: "Thiago", points: 70, categorie: "intermediate" },
-  { alias: "Alves", name: "Djozemara", points: 70, categorie: "intermediate" },
-  { alias: "Serjão", name: "Sergio", points: 70, categorie: "intermediate" },
-];
+import { players } from "../../data/leaderboard";
 
 export function Leaderboard() {
   const isWideVersion = useBreakpointValue({
@@ -59,11 +27,17 @@ export function Leaderboard() {
     lg: true,
   });
 
-  const [categorie, setCategorie] = useState("rx");
+  const [category, setCategory] = useState("rx");
 
-  const playersFiltred = players.filter(
-    (player) => player.categorie === categorie
-  );
+  const playersFiltred = players
+    .filter((player) => player.category === category)
+    .map((player) => {
+      return {
+        ...player,
+        rank: player.points * player.factor,
+      };
+    })
+    .sort((a, b) => (a.rank < b.rank ? -1 : 1));
 
   function formatName(fullName: string, alias: string) {
     return fullName + " (" + alias + ")";
@@ -102,18 +76,18 @@ export function Leaderboard() {
             <Stack direction="row" spacing={4}>
               <Button
                 colorScheme="purple"
-                variant={categorie === "rx" ? "solid" : "outline"}
+                variant={category === "rx" ? "solid" : "outline"}
                 onClick={() => {
-                  setCategorie("rx");
+                  setCategory("rx");
                 }}
               >
                 RX
               </Button>
               <Button
                 colorScheme="purple"
-                variant={categorie === "intermediate" ? "solid" : "outline"}
+                variant={category === "intermediate" ? "solid" : "outline"}
                 onClick={() => {
-                  setCategorie("intermediate");
+                  setCategory("intermediate");
                 }}
               >
                 Intermediate
