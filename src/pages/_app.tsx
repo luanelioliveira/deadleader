ce30bbe7965aa08f7b922ca6fc805c275db3a337
Luan Oliveira <9741553+luanelioliveira@users.noreply.github.com>
2022-08-09 16:40:05 -0300

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,37 @@
-import { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from '../styles/theme';
-import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
+
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Analytics from "../components/Analytics";
 
 function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <ChakraProvider theme={theme} >
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
-  )
+    <>
+      <Analytics />
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default App
+export default App;
