206274961cad0c595c643c979aa0cc422b449a36
Luan Oliveira <9741553+luanelioliveira@users.noreply.github.com>
2022-08-08 23:15:04 -0300

    update leaderboard

--- src/components/Leaderboard/index.tsx
+++ src/components/Leaderboard/index.tsx
@@ -1,82 +1,156 @@
 import { useState } from "react";
 
-import { Avatar, Box, Button, Flex, Stack, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Stack,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useBreakpointValue,
+  VStack,
+} from "@chakra-ui/react";
 
-
-import team12Logo from '../../assets/team12.jpeg'
+import team12Logo from "../../assets/team12.jpeg";
 
 const players = [
-  { name: 'Luan Eli Oliveira', points: 140, categorie: 'rx' },
-  { name: 'Luan Eli Oliveira', points: 135, categorie: 'rx' },
-  { name: 'Luan Eli Oliveira', points: 130, categorie: 'rx' },
-  { name: 'Luan Eli Oliveira', points: 125, categorie: 'rx' },
-  { name: 'Luan Eli Oliveira', points: 120, categorie: 'rx' },
-  { name: 'Luan Eli Oliveira', points: 115, categorie: 'rx' },
-  { name: 'Luan Eli Oliveira', points: 110, categorie: 'rx' },
-  { name: 'Luan Eli Oliveira', points: 105, categorie: 'rx' },
-  { name: 'Luan Eli Oliveira', points: 100, categorie: 'rx' },
-  { name: 'Luan Eli Oliveira', points: 95, categorie: 'rx' },
-  { name: 'Luan Eli Oliveira', points: 90, categorie: 'rx' },
-  { name: 'Luan Eli Oliveira', points: 100, categorie: 'intermediate' },
-  { name: 'Luan Eli Oliveira', points: 90, categorie: 'intermediate' },
-  { name: 'Luan Eli Oliveira', points: 85, categorie: 'intermediate' },
-  { name: 'Luan Eli Oliveira', points: 80, categorie: 'intermediate' },
-  { name: 'Luan Eli Oliveira', points: 75, categorie: 'intermediate' },
-  { name: 'Luan Eli Oliveira', points: 70, categorie: 'intermediate' }
-]
+  { alias: "Racco", name: "Jonatas", points: 140, categorie: "rx" },
+  { alias: "Fano", name: "Estafano", points: 135, categorie: "rx" },
+  { alias: "Anão", name: "Diego", points: 130, categorie: "rx" },
+  { alias: "AeroXimiti", name: "Guilherme", points: 125, categorie: "rx" },
+  { alias: "Steffens", name: "Rafael", points: 120, categorie: "rx" },
+  { alias: "Moreli", name: "Bruno", points: 115, categorie: "rx" },
+  { alias: "Frank", name: "Rodrigo", points: 110, categorie: "rx" },
+  { alias: "Pepe", name: "Felipe", points: 105, categorie: "rx" },
+  { alias: "Show", name: "Bruno", points: 100, categorie: "rx" },
+  { alias: "Sagin", name: "Paula", points: 95, categorie: "rx" },
+  { alias: "Lari", name: "Larissa", points: 90, categorie: "rx" },
+  { alias: "Coach", name: "Luiz", points: 100, categorie: "rx" },
+  { alias: "Metal", name: "Camila", points: 90, categorie: "rx" },
+  { alias: "Kapoti", name: "Bruno", points: 85, categorie: "intermediate" },
+  { alias: "Delai", name: "Fran", points: 80, categorie: "intermediate" },
+  { alias: "Martineli", name: "Diego", points: 75, categorie: "intermediate" },
+  { alias: "Coach", name: "Michel", points: 70, categorie: "intermediate" },
+  { alias: "Desafio", name: "Rodrigo", points: 70, categorie: "intermediate" },
+  { alias: "Guima", name: "Guilherme", points: 70, categorie: "intermediate" },
+  { alias: "Delai", name: "Diogo", points: 70, categorie: "intermediate" },
+  { alias: "Silva", name: "Sabrina", points: 70, categorie: "intermediate" },
+  { alias: "Bandeira", name: "Aline", points: 70, categorie: "intermediate" },
+  {
+    alias: "CapotaAberta",
+    name: "João",
+    points: 70,
+    categorie: "intermediate",
+  },
+  { alias: "Bebidas", name: "Thiago", points: 70, categorie: "intermediate" },
+  { alias: "Alves", name: "Djozemara", points: 70, categorie: "intermediate" },
+  { alias: "Serjão", name: "Sergio", points: 70, categorie: "intermediate" },
+];
 
 export function Leaderboard() {
-  const [categorie, setCategorie] = useState('rx')
-  
-  const playersFiltred = players.filter((player) => player.categorie === categorie)
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
+  const [categorie, setCategorie] = useState("rx");
+
+  const playersFiltred = players.filter(
+    (player) => player.categorie === categorie
+  );
+
+  function formatName(fullName: string, alias: string) {
+    return fullName + " (" + alias + ")";
+  }
+
+  function formatPosition(position) {
+    if (position === 0) return "🥇";
+    if (position === 1) return "🥈";
+    if (position === 2) return "🥉";
+    return position + 1;
+  }
 
   return (
-    <Flex w="100%" align="center" direction="column" p={6}>
+    <VStack
+      align="stretch"
+      my={6}
+      maxWidth={900}
+      mx="auto"
+      bg="gray.800"
+      borderRadius={8}
+      spacing={2}
+    >
+      <Box>
+        <VStack spacing={4} p={8}>
+          <Box>
+            <Avatar size="2xl" src={team12Logo.src} />
+          </Box>
 
-      <Box mt={4}>
-        <Avatar size="2xl" src={team12Logo.src} />
+          <Box>
+            <Text fontSize="4xl" fontWeight="bold" letterSpacing="tight">
+              Team 12AM
+            </Text>
+          </Box>
+
+          <Box>
+            <Stack direction="row" spacing={4}>
+              <Button
+                colorScheme="purple"
+                variant={categorie === "rx" ? "solid" : "outline"}
+                onClick={() => {
+                  setCategorie("rx");
+                }}
+              >
+                RX
+              </Button>
+              <Button
+                colorScheme="purple"
+                variant={categorie === "intermediate" ? "solid" : "outline"}
+                onClick={() => {
+                  setCategorie("intermediate");
+                }}
+              >
+                Intermediate
+              </Button>
+            </Stack>
+          </Box>
+        </VStack>
       </Box>
 
-      <Box mt={6}>
-        <Text fontSize="4xl" fontWeight="bold" letterSpacing="tight">
-          Leaderboard
-        </Text>
-      </Box>
-
-      <Box mt={6}>
-        <Stack direction='row' spacing={4}>
-          <Button colorScheme='purple' variant={categorie === 'rx' ? 'solid' : 'outline'} onClick={() => { setCategorie('rx') }}>
-            RX
-          </Button>
-          <Button colorScheme='purple' variant={categorie === 'intermediate' ? 'solid' : 'outline'} onClick={() => { setCategorie('intermediate') }}>
-            Intermediate
-          </Button>
-        </Stack>
-      </Box>
-
-      <Box mt={6} w="100%" minHeight={200} maxWidth={600}>
+      <Box px={2}>
         <Table colorScheme="whiteAlpha">
           <Thead>
             <Tr>
-              <Th w={4}>Position</Th>
-              <Th>Athlete</Th>
-              <Th>Points</Th>
+              <Th px={5}>#</Th>
+              <Th px={4} w="90%">
+                Athlete
+              </Th>
+              <Th px={4}>Points</Th>
             </Tr>
           </Thead>
           <Tbody>
             {playersFiltred.map((player, position) => {
               return (
                 <Tr>
-                  <Td><Text>#{position+1}</Text></Td>
-                  <Td><Text>{player.name}</Text></Td>
-                  <Td><Text>{player.points}</Text></Td>
+                  <Td px={4}>{formatPosition(position)}</Td>
+                  <Td px={4} w="90%">
+                    {formatName(player.name, player.alias)}
+                  </Td>
+                  <Td px={4} isNumeric>
+                    {player.points}
+                  </Td>
                 </Tr>
-              )
+              );
             })}
-            
           </Tbody>
         </Table>
       </Box>
-    </Flex>
+    </VStack>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,22 @@
-import { Avatar, Box, Button, Flex, Icon, Text, useBreakpointValue, VStack } from "@chakra-ui/react"
-import Link from "next/link";
+import { Box, Button, Flex } from "@chakra-ui/react";
+
 import { Logo } from "../components/Header/Logo";
 
 import { Leaderboard } from "../components/Leaderboard";
 
 export default function Home() {
   return (
-    <Flex direction="column">
-      <Flex as="header" w="100%" maxWidth={1480} h={20} mx="auto" mt={2} px={[6, 8]} align="center">
+    <Box px={4} w="100%" maxWidth={900} mx="auto">
+      <Flex as="header" h={20} align="center">
         <Logo />
         <Flex align="center" ml="auto">
           <Button as="a" colorScheme="pink">
-              Contact Us
+            Contact Us
           </Button>
-        </Flex>  
-      </Flex>
-
-      <Flex w="100%" minHeight={300} maxWidth={1480} mx="auto" px={[6, 8]} py={6}>
-        <Flex w="100%" bg="gray.800" borderRadius={8}>
-          <Leaderboard />
         </Flex>
       </Flex>
-    </Flex>
-  )
+
+      <Leaderboard />
+    </Box>
+  );
 }
